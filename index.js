0d241902e6fbce48b611721d40703c705201d372
Kizito Njoku <nkizito57@gmail.com>
2020-03-03 17:08:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function handleClick(e) {
 function greet() {
 var time = new Date('March 13, 08 01:00').getHours()
 var hours = new Date('March 13, 08 12:00').getHours()
-  for(let e = 1; e < 18; e++) {
+  for(let e = 1; e < 24; e++) {
     if(time < hours) {
       return "Good Morning";
     } else if (time+e > hours && time+e < hours+5){
